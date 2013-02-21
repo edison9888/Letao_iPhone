11409ac93c4bc943d68072790f46a5e76b6fb1af
Kaibin <ekaibwu@gmail.com>
2013-02-21 20:02:11 +0800

    gistfile

--- gistfile2.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-git filter-branch -f --env-filter "
-GIT_AUTHOR_NAME='Kaibin'; 
-GIT_AUTHOR_EMAIL='ekaibwu@gmail.com'; 
-GIT_COMMITER_NAME='Kaibin'; 
-GIT_COMMITTER_EMAIL='ekaibwu@gmail.com';" 
-HEAD
